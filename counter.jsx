623ee0357c8eb89a490ba54a8684a24cd60a0202
kavyaprasad2616-spec <your-email-used-in-that-account>
2026-04-08 14:35:36 +0530

    Added counter.jsx

---
+++ counter.jsx
@@ -0,0 +1,24 @@
+
+import React, { useState } from "react";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount((prev) => prev + 1);
+  };
+
+  const handleDecrement = () => {
+    setCount((prev) => prev - 1);
+  };
+
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <h2>{count}</h2>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+    </div>
+  );
+}
+
